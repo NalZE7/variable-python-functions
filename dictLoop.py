39f792f1887c7e08e417b7fe31b2280e5c49dcc6
NalZE7 <nabilnalze7@gmail.com>
2022-06-30 19:41:21 +0300

    edit one func

--- dictLoop.py
+++ dictLoop.py
@@ -1,37 +1,46 @@
+import os
+
+
+baseDirectory = "/opt/variable-python-functions"
+
+
 def collection11():
-    print("A watermelon's coming right up")
+    path = baseDirectory + "/Category1/Collection11"
+
+    dnsMenuCommand = "/usr/bin/python3 {}/script11.py"
+    os.system(dnsMenuCommand.format(path))
 
 
 def collection12():
-    print("A watermelon's coming right up")
+    print("script12")
 
 
 def collection13():
-    print("A watermelon's coming right up")
+    print("script13")
 
 
 def collection21():
-    print("A watermelon's coming right up")
+    print("script21")
 
 
 def collection22():
-    print("A watermelon's coming right up")
+    print("script22")
 
 
 def collection23():
-    print("A watermelon's coming right up")
+    print("script23")
 
 
 def collection31():
-    print("A watermelon's coming right up")
+    print("script31")
 
 
 def collection32():
-    print("A watermelon's coming right up")
+    print("script32")
 
 
 def collection33():
-    print("A watermelon's coming right up")
+    print("script33")
 
 
 dictOfFood = {collection11: 'script11',
@@ -53,3 +62,11 @@ try:
     function()
 except:
     print("Invalid Input")
+
+    dnsMenuCommand = "/usr/bin/python3 {}/dns_menu.py"
+    os.system(dnsMenuCommand.format(path))
+
+    newSiteCommand = "ansible-playbook {}/newSiteCreation.yml -e \"dest_server={} db_host={} db_password={} dest_bench={} site_name={} site_environment={} public_site_ip={} site_ip={} firewall_hostname={} proxy_server={}\""
+    os.system(newSiteCommand.format(path, destServer, dbHost, dbPassword, destBench,
+                                    siteName, siteEnvironment, sitePublicIP, siteLocalIP, firewallHostname, proxyServer))
+    # print(newSiteCommand.format(path, destServer, dbHost, dbPassword, destBench, siteName, siteEnvironment, sitePublicIP, siteLocalIP, firewallHostname, proxyServer))
