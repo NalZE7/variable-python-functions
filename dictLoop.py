eaab088c97dc822528b47d91c90dbe0fcf262861
NalZE7 <nabilnalze7@gmail.com>
2022-06-30 19:49:02 +0300

    edit functions content

--- dictLoop.py
+++ dictLoop.py
@@ -12,35 +12,59 @@ def collection11():
 
 
 def collection12():
-    print("script12")
+    path = baseDirectory + "/Category1/Collection12"
+
+    dnsMenuCommand = "/usr/bin/python3 {}/script12.py"
+    os.system(dnsMenuCommand.format(path))
 
 
 def collection13():
-    print("script13")
+    path = baseDirectory + "/Category1/Collection13"
+
+    dnsMenuCommand = "/usr/bin/python3 {}/script13.py"
+    os.system(dnsMenuCommand.format(path))
 
 
 def collection21():
-    print("script21")
+    path = baseDirectory + "/Category2/Collection21"
+
+    dnsMenuCommand = "/usr/bin/python3 {}/script21.py"
+    os.system(dnsMenuCommand.format(path))
 
 
 def collection22():
-    print("script22")
+    path = baseDirectory + "/Category2/Collection22"
+
+    dnsMenuCommand = "/usr/bin/python3 {}/script22.py"
+    os.system(dnsMenuCommand.format(path))
 
 
 def collection23():
-    print("script23")
+    path = baseDirectory + "/Category2/Collection23"
+
+    dnsMenuCommand = "/usr/bin/python3 {}/script23.py"
+    os.system(dnsMenuCommand.format(path))
 
 
 def collection31():
-    print("script31")
+    path = baseDirectory + "/Category3/Collection31"
+
+    dnsMenuCommand = "/usr/bin/python3 {}/script31.py"
+    os.system(dnsMenuCommand.format(path))
 
 
 def collection32():
-    print("script32")
+    path = baseDirectory + "/Category3/Collection32"
+
+    dnsMenuCommand = "/usr/bin/python3 {}/script32.py"
+    os.system(dnsMenuCommand.format(path))
 
 
 def collection33():
-    print("script33")
+    path = baseDirectory + "/Category3/Collection33"
+
+    dnsMenuCommand = "/usr/bin/python3 {}/script33.py"
+    os.system(dnsMenuCommand.format(path))
 
 
 dictOfFood = {collection11: 'script11',
